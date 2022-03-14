489e243d7d32d54ec8b2151db37b3094b7327af7
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-03-14 18:33:11 +0300

    addded 19_2 calc

--- lesson 19/19_2.rb
+++ lesson 19/19_2.rb
@@ -1,1 +1,28 @@
 
+puts "First number:"
+number_1 = STDIN.gets.to_f
+puts "Second number:"
+number_2 = STDIN.gets.to_f
+puts "Please choose the operation (+ - * /):"
+operation = STDIN.gets.chomp
+
+result =
+  case operation
+  when "+"
+    number_1 + number_2
+  when "-"
+    number_1 - number_2
+  when "*"
+    (number_1 * number_2).round(2)
+  when "/"
+    if number_2.zero? == false
+      (number_1 / number_2).round(2)
+      else
+      "Infinity!"
+    end
+  else
+    "Operation is incorrect!"
+  end
+
+puts result
+
