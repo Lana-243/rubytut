c16432c51cbd5addb212409663222508612d2d47
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-03-16 22:03:45 +0300

    changed file name time expression

--- lesson 17/17_2/17_2.rb
+++ lesson 17/17_2/17_2.rb
@@ -1,16 +1,17 @@
 current_path = File.dirname(__FILE__)
 
-face_files = ['/data/forehead.txt', '/data/eyes.txt', '/data/nose.txt', '/data/mouth.txt']
-face_paths = face_files.map { |file| current_path + file}
+face_files = ['forehead', 'eyes', 'nose', 'mouth']
+face_paths = face_files.map { |file| "#{current_path}/data/#{file}.txt"}
 face = []
-face_paths.each_with_index do |face_part, index|
-  f = File.new(face_part)
-  face << f.readlines.sample
-  f.close
-end
 
-file_name = "face_#{Time.now.strftime("%d.%m.%Y")}_#{Time.now.strftime("%H.%M.%S")}"
-file = File.new("#{current_path}/#{file_name}.txt")
+face = face_paths.map do |face_part|
+          f = File.new(face_part)
+          face_part = f.readlines.sample
+          f.close
+        end
+
+file_name = Time.now.strftime("face_%d.%m.%Y_%H.%M.%S")
+file = File.new("#{current_path}/#{file_name}.txt", "w")
 
 face.each do |face_part|
   file.puts(face_part)
