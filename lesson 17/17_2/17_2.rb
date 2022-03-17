59f02b4c304b0782935c7f2291dc1d08a033c2ad
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-03-17 11:30:39 +0300

    changed file readlines logic

--- lesson 17/17_2/17_2.rb
+++ lesson 17/17_2/17_2.rb
@@ -4,11 +4,7 @@ face_files = ['forehead', 'eyes', 'nose', 'mouth']
 face_paths = face_files.map { |file| "#{current_path}/data/#{file}.txt"}
 face = []
 
-face = face_paths.map do |face_part|
-          f = File.new(face_part)
-          face_part = f.readlines.sample
-          f.close
-        end
+face = face_paths.map { |filename| File.readlines(filename, chomp: true).sample }
 
 file_name = Time.now.strftime("face_%d.%m.%Y_%H.%M.%S")
 file = File.new("#{current_path}/#{file_name}.txt", "w")
