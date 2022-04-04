6bd0486f419127a11a11d06bd119e62a41ef7d3a
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-04-04 16:45:18 +0500

    override of class type

--- lesson 26/26_2/lib/movie_4.rb
+++ lesson 26/26_2/lib/movie_4.rb
@@ -1,4 +1,4 @@
-class Movie < Item
+class Movie_4 < Item
   attr_reader :title, :price, :quantity
   def initialize(title, price, quantity)
     super
--- lesson 26/26_2/main.rb
+++ lesson 26/26_2/main.rb
@@ -1,7 +1,7 @@
 require_relative 'lib/item'
 require_relative 'lib/book'
-require_relative 'lib/movie'
+require_relative 'lib/movie_4'
 
-a = Movie.new("Leon", 3, 2)
+a = Movie_4.new("Leon", 3, 2)
 
 puts "Movie #{a.title} costs #{a.price}"
