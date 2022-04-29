02cbd80f8cd62c50261fbac365d48026af400aea
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-04-29 13:50:38 +0300

    created forecast class

--- lesson 35/35_1/forecast.rb
+++ lesson 35/35_1/forecast.rb
@@ -1,25 +1,23 @@
 class Forecast
 
+  attr_reader :city_name
+
   CLOUDINESS = %w[Ясно Малооблачно Облачно Пасмурно].freeze
+
   def self.forecast_reader(url)
     uri = URI.parse(url)
     response = Net::HTTP.get_response(uri)
     doc = REXML::Document.new(response.body)
-
-    new.(doc)
+    new(doc)
   end
 
-  def initialize
-
+  def initialize(doc)
+    @doc = doc
   end
-
-
-  doc = REXML::Document.new(response.body)
-
-
 
   def city_name
     URI.decode_www_form_component(
     @doc.root.elements['REPORT/TOWN'].attributes['sname']
   )
+  end
 end
--- lesson 35/35_1/main.rb
+++ lesson 35/35_1/main.rb
@@ -1,30 +1,34 @@
 require 'uri'
 require 'net/http'
 require 'rexml/document'
+require_relative 'forecast'
 
-CLOUDINESS = %w[Ясно Малооблачно Облачно Пасмурно].freeze
-
-uri = URI.parse('https://www.meteoservice.ru/en/export/gismeteo?point=37')
-
-response = Net::HTTP.get_response(uri)
-
-doc = REXML::Document.new(response.body)
-
-city_name = URI.decode_www_form_component(
-  doc.root.elements['REPORT/TOWN'].attributes['sname']
-)
-
-forecast = doc.root.elements['REPORT/TOWN/FORECAST']
-
-min_temp = forecast.elements['TEMPERATURE'].attributes['min']
-max_temp = forecast.elements['TEMPERATURE'].attributes['max']
-
-max_wind = forecast.elements['WIND'].attributes['max']
-
-clouds_index = forecast.elements['PHENOMENA'].attributes['cloudiness'].to_i
-clouds = CLOUDINESS[clouds_index]
-
-puts city_name
-puts "Температура — от #{min_temp} до #{max_temp} С"
-puts "Ветер #{max_wind} м/с"
-puts clouds
+# CLOUDINESS = %w[Ясно Малооблачно Облачно Пасмурно].freeze
+#
+# uri = URI.parse('https://www.meteoservice.ru/en/export/gismeteo?point=37')
+#
+# response = Net::HTTP.get_response(uri)
+#
+# doc = REXML::Document.new(response.body)
+#
+# city_name = URI.decode_www_form_component(
+#   doc.root.elements['REPORT/TOWN'].attributes['sname']
+# )
+#
+# forecast = doc.root.elements['REPORT/TOWN/FORECAST']
+#
+# min_temp = forecast.elements['TEMPERATURE'].attributes['min']
+# max_temp = forecast.elements['TEMPERATURE'].attributes['max']
+#
+# max_wind = forecast.elements['WIND'].attributes['max']
+#
+# clouds_index = forecast.elements['PHENOMENA'].attributes['cloudiness'].to_i
+# clouds = CLOUDINESS[clouds_index]
+#
+# puts city_name
+# puts "Температура — от #{min_temp} до #{max_temp} С"
+# puts "Ветер #{max_wind} м/с"
+# puts clouds
+#
+a = Forecast.forecast_reader('https://www.meteoservice.ru/en/export/gismeteo?point=37')
+puts a.city_name
