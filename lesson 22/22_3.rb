9d66259b5fef27b822184998c29322a18572e341
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-05-01 18:53:41 +0300

    sha2

--- lesson 22/22_3.rb
+++ lesson 22/22_3.rb
@@ -6,12 +6,14 @@ answer = STDIN.gets.chomp
 puts 'How should we cipher it?'
 puts '1. MD5'
 puts '2. SHA1'
+puts '3. SHA2'
+
 
 answer_number = STDIN.gets.to_i
 
 puts 'Here is the result:'
-if answer_number == 1
-  puts Digest::MD5.hexdigest(answer)
-elsif answer_number == 2
-  puts Digest::SHA1.hexdigest(answer)
+case answer_number
+when 1 then puts Digest::MD5.hexdigest(answer)
+when 2 then puts Digest::SHA1.hexdigest(answer)
+when 3 then puts Digest::SHA2.hexdigest(answer)
 end
