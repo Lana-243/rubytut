093595d53d7d8a410af3115c06dfc7625d45a464
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-05-27 10:41:18 +0300

    changed logic for wardrobe

--- lesson 30/30_3/lib/wardrobe.rb
+++ lesson 30/30_3/lib/wardrobe.rb
@@ -3,13 +3,14 @@ require_relative 'garment'
 class Wardrobe
   def self.from_files(filenames)
     wardrobe = filenames.map { |filename| File.readlines(filename, chomp: true) }
-    self.new(wardrobe)
+    clothes = wardrobe.map { |title, type, temperature_range| Garment.new(title, type, temperature_range) }
+    self.new(clothes)
   end
 
   attr_reader :clothes
 
-  def initialize(wardrobe)
-    @clothes = wardrobe.map { |title, type, temperature_range| Garment.new(title, type, temperature_range) }
+  def initialize(clothes)
+    @clothes = clothes
   end
 
   def suitable_garments(temperature)
